429ebd960218fcba885005b3caf3cd1317217e28
Adrian <adrian_george28@yahoo.com>
2021-08-18 19:53:13 +0100

    Add a GET Bills integration test
    I use  Use the Dashboard integration test (indicated with comments) as a guide(test/dasboard.js)

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,7 +2,10 @@ import { screen } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js";
+import ErrorPage from "../views/ErrorPage.js";
 import { localStorageMock } from "../__mocks__/localStorage";
+import LoadingPage from "../views/LoadingPage.js";
+import firebase from "../__mocks__/firebase";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -25,6 +28,38 @@ describe("Given I am connected as an employee", () => {
       let antiChrono = (b, a) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+  });
+
+  // Add a GET Bills integration test FROM tests/Dasboard.js
+
+  describe("Given I am a user connected as Employee", () => {
+    describe("When I navigate to Bills Overview(BillS UI", () => {
+      const getSpy = jest.spyOn(firebase, "get");
+
+      test("fetches bills from mock API GET", async () => {
+        const bills = await firebase.get();
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(bills.data.length).toBe(4);
+      });
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        firebase.get.mockImplementationOnce(() => {
+          Promise.reject(new Error("Error 404"));
+        });
+        const html = BillsUI({ error: "Error 404" });
+        document.body.innerHTML = html;
+        const message = await screen.getByText(/Error 404/);
+        expect(message).toBeTruthy();
+      });
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Error 500"))
+        );
+        const html = BillsUI({ error: "Error 500" });
+        document.body.innerHTML = html;
+        const message = await screen.getByText(/Error 500/);
+        expect(message).toBeTruthy();
+      });
     });
   });
 });
